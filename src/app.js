7b514b02354d5fa1578366c3c8daf84e8937653e
Ivan Radojevic <ivan.radojevic@soneco.co.rs>
2018-03-02 17:50:45 +0100

    First prototype

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './components/Application';
+import './styles/styles.scss'
+import 'normalize.css/normalize.css';
+
+ReactDOM.render(<Application/> , document.getElementById('app'));
---
+++ src/components/Application.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Application extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Empty React app with webpack!
+                </h1>
+            </div>
+        )
+    };
+};
